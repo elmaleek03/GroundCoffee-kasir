a4f30200b4d1160411c36b787f4f6776f0ced859
elmaleek03 <fajarmalik.45@gmail.com>
2021-07-09 21:08:12 +0700

    sidebar not collapsed automatically

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -5,11 +5,11 @@
 </head>
 
 <body id="page-top">
-	<div id="wrapper" >
+	<div id="wrapper"  >
 		<!-- load sidebar -->
 		<?php $this->load->view('partials/sidebar.php') ?>
 
-		<div id="content-wrapper" class="d-flex flex-column">
+		<div id="content-wrapper" class="d-flex flex-column" >
 			<div id="content" data-url="<?= base_url('kasir') ?>">
 				<!-- load Topbar -->
 				<?php $this->load->view('partials/topbar.php') ?>
@@ -42,20 +42,20 @@
 						<div class="card-body">
 							<div class="row" >
 
-								<div class="col-xl-3 col-md-6 mb-4">
-								<div class="card border-left-danger shadow h-100 py-2">
-									<div class="card-body">
-									<div class="row no-gutters align-items-center">
-										<div class="col mr-2">
-										<div class="text-xs font-weight-bold text-danger text-uppercase mb-1">Pembelian Hari Ini</div>
-										<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $total_pembelianperday ?></div>
+								<div class="col-xl-3 col-md-6 mb-4"  >
+									<div class="card border-left-danger shadow h-100 py-2" >
+										<div class="card-body" >
+										<div class="row no-gutters align-items-center" >
+											<div class="col mr-2">
+											<div class="text-xs font-weight-bold text-danger text-uppercase mb-1" >Pembelian Hari Ini</div>
+											<div class="h5 mb-0 font-weight-bold text-gray-800"><?= $total_pembelianperday ?></div>
+											</div>
+											<div class="col-auto">
+											<i class="fas fa-file-invoice fa-2x text-gray-300"></i>
+											</div>
 										</div>
-										<div class="col-auto">
-										<i class="fas fa-file-invoice fa-2x text-gray-300"></i>
 										</div>
 									</div>
-									</div>
-								</div>
 								</div>
 
 								<div class="col-xl-3 col-md-6 mb-4">
